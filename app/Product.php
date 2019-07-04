e4abf6c3c6a1d5b39c213e6e9f627d4dd3a68150
Web <web@it.step>
2019-07-04 19:42:48 +0300

    admin lte implement

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-
+    protected $fillable = [
+      'name', 'url', 'image', 'content', 'price', 'cat_id', 'stock', 'active'
+    ];
 }
